907b4d9620faca995eb4013de0b57d3157b5e4af
PolishedPro Cleaners <info@polishedprocleaners.net>
2025-10-24 17:29:18 -0500

    pixel meta-google tag manager DELETE

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -45,47 +45,7 @@ export default function RootLayout({ children }) {
       </head>
       <body className="min-h-dvh flex flex-col bg-[--color-surface] text-[--color-text-primary] pt-20">
 
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NF67MJDV');`}
-        </Script>
-
-        <Script id="facebook-pixel" strategy="afterInteractive">
-          {`
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window, document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', '1224767959673506');
-        fbq('track', 'PageView');
-          `}
-        </Script>
-
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NF67MJDV"
-            height="0"
-            width="0"
-            style="display:none;visibility:hidden"
-          ></iframe>
-        </noscript>
-
-        <noscript>
-          <img
-            height="1"
-            width="1"
-            style="display:none"
-            src="https://www.facebook.com/tr?id=1224767959673506&ev=PageView&noscript=1"
-          />
-        </noscript>
-
+     
         <Navbar />
         <main className="flex-1">{children}</main>
 
