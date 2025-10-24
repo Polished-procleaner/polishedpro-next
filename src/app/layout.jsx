7c66732d78b22ba353e4786190225d31544e7b03
PolishedPro Cleaners <info@polishedprocleaners.net>
2025-10-24 17:15:09 -0500

    pixel meta-google tag manager 2

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -44,13 +44,14 @@ export default function RootLayout({ children }) {
         />
       </head>
       <body className="min-h-dvh flex flex-col bg-[--color-surface] text-[--color-text-primary] pt-20">
-        <script id="gtm-script" strategy="afterInteractive">
+
+        <Script id="gtm-script" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
           })(window,document,'script','dataLayer','GTM-NF67MJDV');`}
-        </script>
+        </Script>
 
         <Script id="facebook-pixel" strategy="afterInteractive">
           {`
