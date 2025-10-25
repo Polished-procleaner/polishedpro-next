6ea6512919a864179a77edb2f19db4b395cb7fff
PolishedPro Cleaners <info@polishedprocleaners.net>
2025-10-24 22:33:54 -0500

    pixel  AN

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -46,13 +46,21 @@ export default function RootLayout({ children }) {
       </head>
       <body className="min-h-dvh flex flex-col bg-[--color-surface] text-[--color-text-primary] pt-20">
 
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-NF67MJDV');`}
-        </Script>
+       
+
+
+
+
+
+<Script id="gtm-script" strategy="afterInteractive">{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5NJDNXKD');`}</Script>
+
+
+
+
 
         <Script id="facebook-pixel" strategy="afterInteractive">
           {`
