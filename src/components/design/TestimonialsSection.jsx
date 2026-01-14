8c41cbf42e33212129094716dbd1a6ebf4fd5659
Polished-procleaner <info@polishedprocleaners.net>
2026-01-14 13:32:45 -0500

    Actualizando TestimonialsSection

--- src/components/design/TestimonialsSection.jsx
+++ src/components/design/TestimonialsSection.jsx
@@ -1,10 +1,13 @@
 import TestimonialsSectionClient from "./TestimonialsSectionClient";
 
 export default async function TestimonialsSection() {
-  const response = await fetch(`${process.env.APP_URL}/api/reviews`, {
-    cache: "force-cache",
-    next: { revalidate: 60 * 60 },
-  });
+  const response = await fetch(
+    `https://www.polishedprocleaners.net/api/reviews`,
+    {
+      cache: "force-cache",
+      next: { revalidate: 60 * 60 },
+    }
+  );
   const data = await response.json();
 
   return <TestimonialsSectionClient data={data} />;
