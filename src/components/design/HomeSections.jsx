651b45570d7655a49cf02a819d3ff8b734847c5c
PolishedPro Deploy <polishedpro@deploy.com>
2026-05-10 07:04:52 +0000

    Replace middle carousel image with dentist pic, restore left

--- src/components/design/HomeSections.jsx
+++ src/components/design/HomeSections.jsx
@@ -66,8 +66,8 @@ function WhyChooseUs() {
 
 function OurWorks() {
   const images = [
+    "/images/img7b.jpg",
     "/images/dentist.jpg",
-    "/images/stock11.jpg",
     "/images/img3.jpg",
     "/images/img4.jpg",
     "/images/img5.jpg",
